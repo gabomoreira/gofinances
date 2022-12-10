6803639ea6f49852648b0f229a0990ca428ce833
Gabriel Moreira <gsm.br2003@gmail.com>
2022-12-09 22:58:22 -0300

    initial setup

---
+++ App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+
+import { useFonts, Poppins_400Regular, Poppins_500Medium, Poppins_700Bold } from '@expo-google-fonts/poppins';
+
+import theme from './src/global/styles/theme';
+
+import { Dashboard } from './src/screens/dashborad';
+
+export default function App() {
+  const [fonstLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
+
+  if (!fonstLoaded) {
+    return null;
+  }
+  return (
+    <ThemeProvider theme={theme}>
+      <Dashboard />
+    </ThemeProvider>
+  );
+}
